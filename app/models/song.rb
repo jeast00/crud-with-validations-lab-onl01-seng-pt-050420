4ff531839044701fc944778e9a04be5f0c94af0f
Jonathan East <jonathan_east@ymail.com>
2020-09-16 18:20:29 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,4 +6,13 @@ class Song < ActiveRecord::Base
     message: "Cannot be repeated by the same artist in the same year."
   }
 
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+
 end
